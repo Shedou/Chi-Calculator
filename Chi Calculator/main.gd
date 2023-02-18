a39105c2f9330ea5c82c57369d30fe7a6329efab
Chimbal <chimbal.chimbal@yandex.ru>
2023-02-18 16:58:43 +0400

    v2.1

--- Chi Calculator/main.gd
+++ Chi Calculator/main.gd
@@ -16,15 +16,28 @@
 
 extends Node2D
 
-var main_renderX = 0;
-var main_renderY = 0;
+# Version
+var Version = "v2.1"
 
-var result: float = 0.0;
-
+# Deprecated
 var buffer: String = "";
 var main_string = [];
 
-#func _ready():
+# Calc
+var result: float = 0.0;
+var a: float = 0.0;
+var b: float = 0.0;
+var op = "0";
+var num2 = "0";
+var dot = "0";
+var dot2 = "0";
+var calculated = "0";
+var SQRT = "0";
+
+var temp;
+
+func _ready():
+	$Container/Version.text = Version;
 
 func _process(delta):
 	
@@ -48,38 +61,207 @@ func _process(delta):
 		add_buffer("8");
 	if Input.is_action_just_pressed("numb_9"):
 		add_buffer("9");
+	if Input.is_action_just_pressed("numb_dot"):
+		add_buffer(".");
 	if Input.is_action_just_pressed("op_multiply"):
 		add_buffer(" * ");
 	if Input.is_action_just_pressed("op_divide"):
 		add_buffer(" / ");
+	if Input.is_action_just_pressed("op_plus"):
+		add_buffer(" + ");
+	if Input.is_action_just_pressed("op_minus"):
+		add_buffer(" - ");
 	if Input.is_action_just_pressed("ui_accept"):
-		_on_result_pressed();
+		calc();
+		calculated ="1";
 	if Input.is_action_just_pressed("backspace"):
-		_on_result2_pressed();
-
-func _on_result_pressed():
-	main_string = buffer.split(" ");
-	$Container/TextIO/Text_Input.text = str(main_string);
-	
-	if main_string.has("*") || main_string.has("/"):
-		if main_string[1] == "*":
-			result = float(main_string[0]) * float(main_string[2]);
-		elif main_string[1] == "/":
-			result = float(main_string[0]) / float(main_string[2]);
-		$Container/TextIO/Text_Result.text = str(result);
-	else:
-		pass
-
-func _on_result2_pressed():
+		#clear_input();
+		clear_last();
+	if Input.is_action_just_pressed("ui_cancel"):
+		clear_input();
+		
+func clear_input():
 	buffer = "";
+	op = "0";
+	dot = "0";
+	dot2 = "0";
+	num2 = "0";
+	calculated = "0";
+	SQRT = "0";
 	$Container/TextIO/Text_Input.text = str(buffer);
+
+func clear_last():
+	main_string = buffer.split(" ");
+	if op == "0":
+		temp = str(main_string[0]);
+		temp.erase(temp.length() - 1, 1);
+		buffer = temp;
+		$Container/TextIO/Text_Input.text = str(buffer);
+	if op == "1" && num2 == "0":
+		if main_string.has("*") || main_string.has("/") || main_string.has("-") || main_string.has("+") || main_string.has("%"):
+			temp = str(main_string[0]) + str(main_string[1]);
+			temp.erase(temp.length() - 1, 1);
+			buffer = temp;
+			$Container/TextIO/Text_Input.text = str(buffer);
+	if op == "1" && num2 == "1":
+		if main_string.has("*") || main_string.has("/") || main_string.has("-") || main_string.has("+") || main_string.has("%"):
+			temp = str(main_string[0]) + str(main_string[1]) + str(main_string[2]);
+			temp.erase(temp.length() - 1, 1);
+			buffer = temp;
+			$Container/TextIO/Text_Input.text = str(buffer);
 
 func add_buffer(numb):
-	buffer = buffer + str(numb);
+	buffer = $Container/TextIO/Text_Input.text;
+	if numb == " √ ":
+			buffer = buffer + str(numb);
+			calc_sqrt();
+			buffer = str(result);
+	
+	if numb == " * " || numb == " / " || numb == " + " || numb == " - " || numb == " % ":
+		if op == "0":
+			buffer = buffer + str(numb);
+			op = "1";
+		if op == "1" && num2 == "0":
+			main_string = buffer.split(" ");
+			main_string[1] = numb;
+			buffer = main_string[0] + main_string[1] + main_string[2];
+		if op == "1" && num2 == "1":
+			calc();
+			op == "0"
+			buffer = str(result) + numb;
+	
+	if numb == "0" || numb == "1" || numb == "2" || numb == "3" || numb == "4" || numb == "5" || numb == "6" || numb == "7" || numb == "8" || numb == "9" || numb == ".":
+		if numb != "." && op == "1":
+			num2 = "1";
+		if numb != ".":
+			if calculated == "1":
+				op = "0";
+				dot = "0";
+				dot2 = "0";
+				num2 = "0";
+				buffer = "";
+				calculated = "0";
+				SQRT = "0";
+			buffer += numb;
+		
+		if op == "0" && dot == "0" && numb == ".":
+			dot = "1";
+			buffer += numb;
+		if op == "1" && dot2 == "0" && numb == ".":
+			dot2 = "1";
+			buffer += numb;
+			
 	$Container/TextIO/Text_Input.text = str(buffer);
+	
+func calc_sqrt():
+	main_string = buffer.split(" ");
+	if main_string[1] == "%":
+		result = sqrt(100 * float(main_string[0]) / float(main_string[2]));
+	if main_string[1] == "+":
+		result = sqrt(float(main_string[0]) + float(main_string[2]));
+	if main_string[1] == "-":
+		result = sqrt(float(main_string[0]) - float(main_string[2]));
+	if main_string[1] == "/":
+		result = sqrt(float(main_string[0]) / float(main_string[2]));
+	if main_string[1] == "*":
+		result = sqrt(float(main_string[0]) * float(main_string[2]));
+	if main_string[1] == "√":
+		result = sqrt(float(main_string[0]));
+	$Container/TextIO/Text_Result.text = str(result);
+	
+func calc():
+	buffer = $Container/TextIO/Text_Input.text;
+	main_string = buffer.split(" ");
+	$Container/TextIO/Text_Input.text = str(main_string);
+
+	if main_string.has("*") || main_string.has("/") || main_string.has("-") || main_string.has("+") || main_string.has("%"):
+		if main_string[1] == "*":
+			if main_string[2] != "0" && main_string[2]:
+				result = float(main_string[0]) * float(main_string[2]);
+		if main_string[1] == "/":
+			if main_string[2] != "0" && main_string[2]:
+				result = float(main_string[0]) / float(main_string[2]);
+		if main_string[1] == "-":
+			result = float(main_string[0]) - float(main_string[2]);
+		if main_string[1] == "+":
+			result = float(main_string[0]) + float(main_string[2]);
+		if main_string[1] == "%":
+			result = float(main_string[2]) / 100 * float(main_string[0]);
+		$Container/TextIO/Text_Result.text = str(result);
 
 func _on_Link_Blog_pressed():
 	OS.shell_open("https://overclockers.ru/blog/Hard-Workshop");
 
 func _on_Link_Git_pressed():
 	OS.shell_open("https://github.com/Shedou/Chi-Calculator");
+
+#
+# Operand
+func _on_sqrt_pressed():
+	add_buffer(" √ ");
+
+func _on_Percent_pressed():
+	add_buffer(" % ");
+
+func _on_Multiple_pressed():
+	add_buffer(" * ");
+
+func _on_Divide_pressed():
+	add_buffer(" / ");
+
+func _on_Plus_pressed():
+	add_buffer(" + ");
+	
+func _on_Minus_pressed():
+	add_buffer(" - ");
+
+func _on_Result_pressed():
+	calc();
+	
+	calculated ="1";
+
+func _on_AC_pressed():
+	clear_input();
+	
+func _on_Del_pressed():
+	clear_last();
+	
+# Operand
+#
+#
+# Numbers
+func _on_Num_1_pressed():
+	add_buffer("1");
+
+func _on_Num_2_pressed():
+	add_buffer("2");
+
+func _on_Num_3_pressed():
+	add_buffer("3");
+
+func _on_Num_4_pressed():
+	add_buffer("4");
+
+func _on_Num_5_pressed():
+	add_buffer("5");
+
+func _on_Num_6_pressed():
+	add_buffer("6");
+
+func _on_Num_7_pressed():
+	add_buffer("7");
+
+func _on_Num_8_pressed():
+	add_buffer("8");
+
+func _on_Num_9_pressed():
+	add_buffer("9");
+	
+func _on_Num_0_pressed():
+	add_buffer("0");
+
+func _on_Num_point_pressed():
+	add_buffer(".");
+	
+# Numbers
+#
