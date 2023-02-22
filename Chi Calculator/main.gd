32f90a0c9e046e6de1c4eea5b8a843ec0822537f
Chimbal <chimbal.chimbal@yandex.ru>
2023-02-22 14:25:23 +0400

    v2.4

--- Chi Calculator/main.gd
+++ Chi Calculator/main.gd
@@ -17,28 +17,177 @@
 extends Node2D
 
 # Version
-var Version = "v2.3"
+var Version = "v2.4"
 
 # Strings
 var chi_str_buffer: String = "";
-var chi_str_temp = [];
+var chi_arr_temp = [];
 
-# trash
-var chi_float_result: float = 0.0;
-var chi_bool_operand: bool = false;
-var chi_bool_numb2: bool = false;
-var chi_bool_dot_1: bool = false;
-var chi_bool_dot_2: bool = false;
-var chi_bool_calculated: bool = false;
-var chi_int_cAC: int = 0;
+# ...
+var chi_float_result: float = 0.0;		# Результат вычислений
+var chi_bool_operand: bool = false;		# Нажат ли операнд "*", "/", "-", "+" и т.п.
+var chi_bool_numb2: bool = false;		# Есть ли второе число
+var chi_bool_dot_1: bool = false;		# Наличие точки в первом числе
+var chi_bool_dot_2: bool = false;		# Наличие точки во втором числе
+var chi_bool_calculated: bool = false;	# Расчитано или нет
 
-var Text_Input_Focus = 0;
+var chi_shift = 0;
 
-var chi_na_temp;
+var chi_int_cAC: int = 0;	# Костыль для работы "C/AC"
+
+var Text_Input_Focus = 0;	# Наведен ли курсор на окно ввода
+
+var chi_na_temp;			# Временная переменная
 
 func _ready():
 	$Container/Version.text = Version;
 
+func add_buffer(numb):
+	chi_int_cAC = 0; # Костыль для правильной работы "C/AC"
+	chi_str_buffer = $Container/TextIO/Text_Input.text;
+	if numb == " √ ":
+			chi_str_buffer = chi_str_buffer + str(numb);
+			calc_sqrt();
+	
+	if numb in [" * ", " / ", " + ", " - ", " % "]:
+		if chi_bool_operand == false:
+			chi_str_buffer = chi_str_buffer + str(numb);
+			chi_bool_operand = true;
+			chi_put_Text_Input(chi_str_buffer);
+		if chi_bool_operand == true && chi_bool_numb2 == false:
+			chi_arr_temp = chi_str_buffer.split(" ");
+			chi_arr_temp[1] = numb;
+			chi_str_buffer = chi_arr_temp[0] + chi_arr_temp[1] + chi_arr_temp[2];
+			chi_put_Text_Input(chi_str_buffer);
+		if chi_bool_operand == true && chi_bool_numb2 == true:
+			chi_calculate();
+			#chi_bool_operand = false;
+			chi_bool_dot_2 = false;
+			chi_str_buffer = str(chi_float_result) + numb;
+			chi_put_Text_Input(chi_str_buffer);
+			
+	if numb == ".":
+		if chi_bool_operand == false && chi_bool_dot_1 == false:
+			chi_bool_dot_1 = true;
+			chi_str_buffer += numb;
+			chi_put_Text_Input(chi_str_buffer);
+		if chi_bool_operand == true && chi_bool_dot_2 == false:
+			chi_bool_dot_2 = true;
+			chi_str_buffer += numb;
+			chi_put_Text_Input(chi_str_buffer);
+			
+	if numb in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] and chi_shift == 0:
+		if chi_bool_operand == true:
+			chi_bool_numb2 = true;
+		chi_str_buffer += numb;
+		chi_put_Text_Input(chi_str_buffer);
+
+#
+# Функция расчета квадратного корня
+func calc_sqrt():
+	chi_int_cAC = 0; # Костыль для правильной работы "C/AC"
+	chi_arr_temp = chi_str_buffer.split(" "); # Разделение строки на элементы и помещение в массив
+	if chi_arr_temp[1] == "%":
+		chi_float_result = sqrt(100 * float(chi_arr_temp[0]) / float(chi_arr_temp[2]));
+	if chi_arr_temp[1] == "+":
+		chi_float_result = sqrt(float(chi_arr_temp[0]) + float(chi_arr_temp[2]));
+	if chi_arr_temp[1] == "-":
+		chi_float_result = sqrt(float(chi_arr_temp[0]) - float(chi_arr_temp[2]));
+	if chi_arr_temp[1] == "/":
+		chi_float_result = sqrt(float(chi_arr_temp[0]) / float(chi_arr_temp[2]));
+	if chi_arr_temp[1] == "*":
+		chi_float_result = sqrt(float(chi_arr_temp[0]) * float(chi_arr_temp[2]));
+	if chi_arr_temp[1] == "√":
+		chi_float_result = sqrt(float(chi_arr_temp[0]));
+	chi_put_Text_Output(chi_float_result);
+	chi_str_buffer = str(chi_float_result);
+
+#
+# Функция общих вычислений
+func chi_calculate():
+	chi_int_cAC = 0; # Костыль для правильной работы "C/AC"
+	chi_str_buffer = $Container/TextIO/Text_Input.text; # Берем выражение из поля ввода если пользователь ввел вручную
+	chi_arr_temp = chi_str_buffer.split(" "); # Разделение строки на элементы и помещение в массив
+	chi_put_Text_Input(chi_arr_temp); # Показ содержимого массива
+
+	if chi_arr_temp.has("*") || chi_arr_temp.has("/") || chi_arr_temp.has("-") || chi_arr_temp.has("+") || chi_arr_temp.has("%"):
+		if chi_arr_temp[1] == "*":
+			if chi_arr_temp[2] != "0" && chi_arr_temp[2]:
+				chi_float_result = float(chi_arr_temp[0]) * float(chi_arr_temp[2]);
+		if chi_arr_temp[1] == "/":
+			if chi_arr_temp[2] != "0" && chi_arr_temp[2]:
+				chi_float_result = float(chi_arr_temp[0]) / float(chi_arr_temp[2]);
+		if chi_arr_temp[1] == "-":
+			chi_float_result = float(chi_arr_temp[0]) - float(chi_arr_temp[2]);
+		if chi_arr_temp[1] == "+":
+			chi_float_result = float(chi_arr_temp[0]) + float(chi_arr_temp[2]);
+		if chi_arr_temp[1] == "%":
+			chi_float_result = float(chi_arr_temp[2]) / 100 * float(chi_arr_temp[0]);
+		chi_put_Text_Output(chi_float_result);
+#
+# Функция для коррекции ввода
+func clear_last():
+	chi_int_cAC = 0; # Костыль для правильной работы "C/AC"
+	chi_arr_temp = chi_str_buffer.split(" "); # Разделение строки на элементы и помещение в массив
+	if chi_bool_operand == false && chi_bool_numb2 == false:
+		chi_na_temp = str(chi_arr_temp[0]);
+		if chi_na_temp.substr(chi_na_temp.length() - 1, 1) == ".":
+			chi_bool_dot_1 = false;
+		chi_na_temp.erase(chi_na_temp.length() - 1, 1);
+		chi_str_buffer = chi_na_temp;
+		chi_put_Text_Input(chi_str_buffer);
+	if chi_bool_operand == true && chi_bool_numb2 == true:
+		chi_na_temp = str(chi_arr_temp[2]);
+		if chi_na_temp.substr(chi_na_temp.length() - 1, 1) == ".":
+			chi_bool_dot_2 = false;
+		chi_na_temp.erase(chi_na_temp.length() - 1, 1);
+		chi_str_buffer = str(chi_arr_temp[0]) + " " +str(chi_arr_temp[1]) + " " + chi_na_temp;
+		chi_put_Text_Input(chi_str_buffer);
+		
+
+#
+# Функция очистки ввода/вывода
+func chi_ClearIO():
+	chi_str_buffer = "";
+	chi_bool_operand = false;
+	chi_bool_dot_1 = false;
+	chi_bool_dot_2 = false
+	chi_bool_numb2 = false;
+	chi_bool_calculated = false;
+	chi_put_Text_Input(chi_str_buffer); # Очистка вывода при повторном нажатии "C/AC"
+	if chi_int_cAC >= 1:
+		chi_put_Text_Output(chi_str_buffer);
+		chi_int_cAC = 0;
+
+#
+# Функция частичного сброса (не для использования пользователем)
+func chi_ClearFlags():
+	chi_str_buffer = "";
+	chi_bool_operand = false;
+	chi_bool_dot_1 = false;
+	chi_bool_dot_2 = false;
+	chi_bool_numb2 = false;
+	chi_bool_calculated = false;
+	chi_int_cAC = 0;
+
+#
+# Функции присваивающие значение полям ввода/вывода
+func chi_put_Text_Input(value):
+	$Container/TextIO/Text_Input.text = str(value);
+	
+func chi_put_Text_Output(value):
+	$Container/TextIO/Text_Result.text = str(value);
+
+#
+# Функции ссылок на блог и репозиторий
+func _on_Link_Blog_pressed():
+	OS.shell_open("https://overclockers.ru/blog/Hard-Workshop");
+
+func _on_Link_Git_pressed():
+	OS.shell_open("https://github.com/Shedou/Chi-Calculator");
+
+#
+# Функция для отлова нажатых кнопок с клавиатуры
 func _unhandled_key_input(event):
 	if Text_Input_Focus == 0:
 		if Input.is_action_just_pressed("numb_0"):
@@ -71,152 +220,22 @@ func _unhandled_key_input(event):
 			add_buffer(" + ");
 		if Input.is_action_just_pressed("op_minus"):
 			add_buffer(" - ");
+		if Input.is_action_just_pressed("op_percent"):
+			add_buffer(" % ");
 		if Input.is_action_just_pressed("ui_accept"):
 			chi_calculate();
-			chi_bool_calculated = false;
 		if Input.is_action_just_pressed("backspace"):
 			clear_last();
 		if Input.is_action_just_pressed("ui_cancel"):
-			clear_input();
+			chi_ClearIO();
 			chi_int_cAC += 1;
-
-#func _process(delta):
-#	pass;
-		
-func clear_input():
-	chi_str_buffer = "";
-	chi_bool_operand = false;
-	chi_bool_dot_1 = false;
-	chi_bool_dot_2 = false
-	chi_bool_numb2 = false;
-	chi_bool_calculated = false;
-	chi_put_Text_Input(chi_str_buffer);
-	if chi_int_cAC >= 1:
-		chi_put_Text_Output(chi_str_buffer);
-		chi_int_cAC = 0;
-
-func clear_flags():
-	chi_str_buffer = "";
-	chi_bool_operand = false;
-	chi_bool_dot_1 = false;
-	chi_bool_dot_2 = false;
-	chi_bool_numb2 = false;
-	chi_bool_calculated = false;
-	chi_int_cAC = 0;
-
-func clear_last():
-	chi_int_cAC = 0;
-	chi_str_temp = chi_str_buffer.split(" ");
-	if chi_bool_operand == false && chi_bool_numb2 == false:
-		chi_na_temp = str(chi_str_temp[0]);
-		if chi_na_temp.substr(chi_na_temp.length() - 1, 1) == ".":
-			chi_bool_dot_1 = false;
-		chi_na_temp.erase(chi_na_temp.length() - 1, 1);
-		chi_str_buffer = chi_na_temp;
-		chi_put_Text_Input(chi_str_buffer);
-	if chi_bool_operand == true && chi_bool_numb2 == true:
-		chi_na_temp = str(chi_str_temp[2]);
-		if chi_na_temp.substr(chi_na_temp.length() - 1, 1) == ".":
-			chi_bool_dot_2 = false;
-		chi_na_temp.erase(chi_na_temp.length() - 1, 1);
-		chi_str_buffer = str(chi_str_temp[0]) + " " +str(chi_str_temp[1]) + " " + chi_na_temp;
-		chi_put_Text_Input(chi_str_buffer);
-
-func add_buffer(numb):
-	chi_int_cAC = 0;
-	chi_str_buffer = $Container/TextIO/Text_Input.text;
-	if numb == " √ ":
-			chi_str_buffer = chi_str_buffer + str(numb);
-			calc_sqrt();
-	
-	if numb in [" * ", " / ", " + ", " - ", " % "]:
-		if chi_bool_operand == false:
-			chi_str_buffer = chi_str_buffer + str(numb);
-			chi_bool_operand = true;
-			chi_put_Text_Input(chi_str_buffer);
-		if chi_bool_operand == true && chi_bool_numb2 == false:
-			chi_str_temp = chi_str_buffer.split(" ");
-			chi_str_temp[1] = numb;
-			chi_str_buffer = chi_str_temp[0] + chi_str_temp[1] + chi_str_temp[2];
-			chi_put_Text_Input(chi_str_buffer);
-		if chi_bool_operand == true && chi_bool_numb2 == true:
-			chi_calculate();
-			chi_bool_operand == false
-			chi_str_buffer = str(chi_float_result) + numb;
-			chi_put_Text_Input(chi_str_buffer);
-	
-	if numb in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:
-		if numb != "." && chi_bool_operand == true:
-			chi_bool_numb2 = true;
-			chi_put_Text_Input(chi_str_buffer);
-		if numb != ".":
-			if chi_bool_calculated == true:
-				clear_flags();
-			chi_str_buffer += numb;
-			chi_put_Text_Input(chi_str_buffer);
-		
-		if chi_bool_operand == false && chi_bool_dot_1 == false && numb == ".":
-			chi_bool_dot_1 = true;
-			chi_str_buffer += numb;
-			chi_put_Text_Input(chi_str_buffer);
-		if chi_bool_operand == true && chi_bool_dot_2 == false && numb == ".":
-			chi_bool_dot_2 = true;
-			chi_str_buffer += numb;
-			chi_put_Text_Input(chi_str_buffer);
-	
-func calc_sqrt():
-	chi_int_cAC = 0;
-	chi_str_temp = chi_str_buffer.split(" ");
-	if chi_str_temp[1] == "%":
-		chi_float_result = sqrt(100 * float(chi_str_temp[0]) / float(chi_str_temp[2]));
-	if chi_str_temp[1] == "+":
-		chi_float_result = sqrt(float(chi_str_temp[0]) + float(chi_str_temp[2]));
-	if chi_str_temp[1] == "-":
-		chi_float_result = sqrt(float(chi_str_temp[0]) - float(chi_str_temp[2]));
-	if chi_str_temp[1] == "/":
-		chi_float_result = sqrt(float(chi_str_temp[0]) / float(chi_str_temp[2]));
-	if chi_str_temp[1] == "*":
-		chi_float_result = sqrt(float(chi_str_temp[0]) * float(chi_str_temp[2]));
-	if chi_str_temp[1] == "√":
-		chi_float_result = sqrt(float(chi_str_temp[0]));
-	chi_put_Text_Output(chi_float_result);
-	chi_str_buffer = str(chi_float_result);
-	
-func chi_put_Text_Input(value):
-	$Container/TextIO/Text_Input.text = str(value);
-	
-func chi_put_Text_Output(value):
-	$Container/TextIO/Text_Result.text = str(value);
-	
-func chi_calculate():
-	chi_int_cAC = 0;
-	chi_str_buffer = $Container/TextIO/Text_Input.text;
-	chi_str_temp = chi_str_buffer.split(" ");
-	chi_put_Text_Input(chi_str_temp);
-
-	if chi_str_temp.has("*") || chi_str_temp.has("/") || chi_str_temp.has("-") || chi_str_temp.has("+") || chi_str_temp.has("%"):
-		if chi_str_temp[1] == "*":
-			if chi_str_temp[2] != "0" && chi_str_temp[2]:
-				chi_float_result = float(chi_str_temp[0]) * float(chi_str_temp[2]);
-		if chi_str_temp[1] == "/":
-			if chi_str_temp[2] != "0" && chi_str_temp[2]:
-				chi_float_result = float(chi_str_temp[0]) / float(chi_str_temp[2]);
-		if chi_str_temp[1] == "-":
-			chi_float_result = float(chi_str_temp[0]) - float(chi_str_temp[2]);
-		if chi_str_temp[1] == "+":
-			chi_float_result = float(chi_str_temp[0]) + float(chi_str_temp[2]);
-		if chi_str_temp[1] == "%":
-			chi_float_result = float(chi_str_temp[2]) / 100 * float(chi_str_temp[0]);
-		chi_put_Text_Output(chi_float_result);
-
-func _on_Link_Blog_pressed():
-	OS.shell_open("https://overclockers.ru/blog/Hard-Workshop");
-
-func _on_Link_Git_pressed():
-	OS.shell_open("https://github.com/Shedou/Chi-Calculator");
+		if Input.is_action_pressed("spec_shift"):
+			chi_shift = 1;
+		else:
+			chi_shift = 0;
 
 #
-# Operand
+# Функции для отлова нажатий по кнопкам интерфейса
 func _on_sqrt_pressed():
 	add_buffer(" √ ");
 
@@ -237,15 +256,14 @@ func _on_Minus_pressed():
 
 func _on_Result_pressed():
 	chi_calculate();
-	chi_bool_calculated = true;
 
 func _on_AC_pressed():
-	clear_input();
+	chi_ClearIO();
 	chi_int_cAC += 1;
 	
 func _on_Del_pressed():
 	clear_last();
-	chi_int_cAC = 0;
+	chi_int_cAC = 0; # Костыль для правильной работы "C/AC"
 	
 # Operand
 #
